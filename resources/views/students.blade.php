173115fbefd582ba85b5312ffd13c69605e65c42
Luca Isidoro <apprendista44@gmail.com>
2020-08-31 18:47:29 +0200

    commit esercizio

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.layout')
 
 @section('main_content')
-  <h1>ciao</h1>
+  <h1>Pagina Students</h1>
+  <div>
+    @foreach ($students as $student)
+      <ul>
+        <li>Nome: {{ $student->nome }}</li>
+        <li>Nome: {{ $student->cognome }}</li>
+        <li>Nome: {{ $student->voto }}</li>
 
+      </ul>
+    @endforeach
+
+  </div>
 @endsection
